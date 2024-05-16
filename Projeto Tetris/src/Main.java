0d8ffdd41257b2c0ef48ac6fcb20648575c1b1db
haitsr <jorgefelipe11_@hotmail.com>
2024-05-15 21:15:31 -0300

    mixed functions in main

--- Projeto Tetris/src/Main.java
+++ Projeto Tetris/src/Main.java
@@ -6,8 +6,57 @@ import Matrix.Pieces.Piece;
 public class Main {
     public static void main(String[] args) {
         Board B = new Board(20, 10);
-
+        System.out.println("This is checking movements of a piece on the board.");
+        PieceMovements(B);
+        Engine.clearScreen();
+        System.out.println("This is deleting a line full of pieces.");
         deletefullline(B);
+        Engine.clearScreen();
+        System.out.println("This is simulating a functional run of the games without proper graphics");
+        Engine.run(B);
+        Engine.clearScreen();
+    }
+    /**
+     * Shows all the movements available for a piece
+     * @param B: Board
+     */
+    public static void PieceMovements(Board B) {
+
+        Piece p = new Jpiece();
+        p.setX(5);
+        p.setY(10);
+
+        B.setFallingPiece(p);
+
+        //Shows the piece on its original position on the board
+        B.update();
+        System.out.println(B);
+        B.clear();
+
+        p.moveLeft();
+        B.update();
+        System.out.println(B);
+        B.clear();
+
+        p.moveRight();
+        B.update();
+        System.out.println(B);
+        B.clear();
+
+        p.rotateClockWise();
+        B.update();
+        System.out.println(B);
+        B.clear();
+
+        p.rotateCounterClockWise();
+        B.update();
+        System.out.println(B);
+        B.clear();
+
+        Engine.DropDown(B, p);
+        B.update();
+        System.out.println(B);
+        B.clear();
     }
     public static void deletefullline(Board B){
         System.out.println(B);
